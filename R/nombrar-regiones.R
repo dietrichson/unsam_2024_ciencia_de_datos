82d9fbf0188be95ed97c285356190cef9921ae7a
Aleksander Dietrichson <dietrichson@gmail.com>
2024-04-22 19:26:19 +0000

    Trabajo en #34

---
+++ R/nombrar-regiones.R
@@ -0,0 +1,12 @@
+# Función para poner etiquetas a las regiones de los datos del INDEC
+
+nombrar_regiones <- function(region){
+  case_when(
+    region == 01 ~ "Gran Buenos Aires",
+    region == 40 ~ "Noroeste",
+    region == 41 ~ "Noreste",
+    region == 42 ~ "Cuyo",
+    region == 43 ~ "Pampeana",
+    region == 44 ~ "Patagonia",
+    .default = "No Encontrado")
+}
