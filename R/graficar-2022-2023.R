b2784d5aa12c21d2a89cfc6900222fc3612e5ecb
Aleksander Dietrichson <dietrichson@gmail.com>
2024-05-13 20:05:51 +0000

    Agrego notas y funcion de grafico

---
+++ R/graficar-2022-2023.R
@@ -0,0 +1,11 @@
+graficar_2022_2023 <- function(my_data){
+  my_data |> 
+    group_by(Año) |> 
+    summarize(Media = median(ITF)) |> 
+    ggplot(aes(Año, Media, fill = Año, group = 1))+
+    geom_col(width = .66)+
+    geom_point(stat="identity", size = 3, color = "gray")+
+    geom_line(lwd = 1, color = "gray")+
+    theme_bw()+
+    theme(legend.position = "none")
+}
