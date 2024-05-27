50cc393dc4c02e80641a97da4bd4ee965073d6a0
Lautaro Huergo <you@example.com>
2024-05-27 17:27:47 +0000

    Agregar Modificación

--- R/función-para-graficar-distribución-ingreso.R
+++ R/función-para-graficar-distribución-ingreso.R
@@ -1,14 +1,17 @@
 #FUNCIÓN PARA GRAFICAR DISTRIBUCIÓN DEL INGRESO
 
 #FUNCIÓN 1:
+distribución_del_ingreso <- function(ITF){
 my_data |> 
   ggplot(aes(ITF))+
   geom_histogram()+
   xlab("Ingreso Total Familiar")+
   ylab("Numero de Hogares")
-
+}
 #FUNCIÓN 2:
+distribución_del_ingreso <- function(ITF){
 my_data |> 
   filter(ITF<1500000) |>
   ggplot(aes(ITF))+
   geom_density()
+}
